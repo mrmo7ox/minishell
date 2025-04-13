e9ff1db186074979270efcb56ab809a054df88d0
mrmo7ox <medelouardisec@gmail.com>
2025-04-13 16:25:27 +0100

    prank

--- parsing/synatx_err/parenthesis.c
+++ parsing/synatx_err/parenthesis.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parenthesis.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ihamani <ihamani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: moel-oua <moel-oua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 13:21:13 by moel-oua          #+#    #+#             */
-/*   Updated: 2025/04/13 10:47:24 by ihamani          ###   ########.fr       */
+/*   Updated: 2025/04/13 16:24:50 by moel-oua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ static bool	is_operator_after(char *line, int *open_count, int *j, int *i)
 	(*j) = (*i) + 1;
 	while (line[*j] && ft_whitespaces(line[(*j)]))
 		(*j)++;
-	if (line[*j] && !ft_chrstr(line[*j], "|&<>"))
+	if (line[*j] && !ft_chrstr(line[*j], "|&<>)"))
 		return (false);
 	(*open_count)--;
 	return (true);
