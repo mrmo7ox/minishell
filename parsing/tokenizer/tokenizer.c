ed763b288c7f77ad35325e731e0ea17e1f3bcb42
nerovanity <hamani.dami.imad@gmail.com>
2025-05-01 11:40:44 +0100

    test

--- parsing/tokenizer/tokenizer.c
+++ parsing/tokenizer/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moel-oua <moel-oua@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ihamani <ihamani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:05:51 by moel-oua          #+#    #+#             */
-/*   Updated: 2025/05/01 11:28:20 by moel-oua         ###   ########.fr       */
+/*   Updated: 2025/05/01 11:40:36 by ihamani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,3 +50,4 @@ bool	tokenizer(t_leaf **root, t_gc **garbage, char *line)
 		return (false);
 	return (true);
 }
+//sadasdasdsasd
