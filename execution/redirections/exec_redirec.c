a106d843583bf0ccbe57f9b5d9f46ab377e12795
Mohamed El Ouardi <moel-oua@e1r8p11.1337.ma>
2025-05-04 11:37:48 +0100

    norm

--- execution/redirections/exec_redirec.c
+++ execution/redirections/exec_redirec.c
@@ -6,12 +6,20 @@
 /*   By: moel-oua <moel-oua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 09:44:05 by moel-oua          #+#    #+#             */
-/*   Updated: 2025/05/04 09:47:41 by moel-oua         ###   ########.fr       */
+/*   Updated: 2025/05/04 11:35:28 by moel-oua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-bool	exec_redirec(t_tk *token)
+bool	exec_redirec(t_tk *token, t_gc **garbage)
 {
+	t_redic	*curr;
+
+	curr = token->redics;
+	while (curr)
+	{
+		printf("[%s]\n", curr->content);
+		curr = curr->next;
+	}
 }
